802a8b122fd003bca7cd8e714730dcf91221c07a
Ciccone-Okodo <cicconjo@den0138m.vertafore.com>
2019-10-21 09:50:04 -0600

    added enum

--- src/test/java/com/vertafore/test/common/models/services/commission/AgencyCommission/AgencyCommissionV1.java
+++ src/test/java/com/vertafore/test/common/models/services/commission/AgencyCommission/AgencyCommissionV1.java
@@ -1,10 +1,11 @@
 package com.vertafore.test.common.models.services.commission.AgencyCommission;
 
 import com.vertafore.test.common.models.services.commission.commission.CommissionV1;
+import com.vertafore.test.common.models.services.commission.commissionEnums.AgencyCommissionLineOfBusiness;
 
 public class AgencyCommissionV1 extends CommissionV1 {
 
     public String carrierId;
-    public String lob;
+    public AgencyCommissionLineOfBusiness lob;
 
 }
---
+++ src/test/java/com/vertafore/test/common/models/services/commission/commissionEnums/AgencyCommissionLineOfBusiness.java
@@ -0,0 +1,5 @@
+package com.vertafore.test.common.models.services.commission.commissionEnums;
+
+public enum AgencyCommissionLineOfBusiness {
+    LIABILITY
+}
