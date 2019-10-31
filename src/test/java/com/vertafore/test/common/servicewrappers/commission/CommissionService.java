5e7617cd1203355cf94572394c2ac800e82c5300
josh <jcicconeokodo@vertafore.com>
2019-10-31 12:49:52 -0600

    added model and servicewrapper endpoints

--- src/test/java/com/vertafore/test/common/servicewrappers/commission/CommissionService.java
+++ src/test/java/com/vertafore/test/common/servicewrappers/commission/CommissionService.java
@@ -3,6 +3,7 @@ package com.vertafore.test.common.servicewrappers.commission;
 import com.vertafore.test.common.models.services.commission.AgencyCommission.AgencyCommissionV1;
 import com.vertafore.test.common.models.services.commission.commissionEnums.AgencyCommissionLineOfBusiness;
 import com.vertafore.test.common.models.services.commission.producerCommission.ProducerCommissionV1;
+import com.vertafore.test.common.models.services.commission.rule.DefaultRuleV1;
 import com.vertafore.test.common.util.ServiceUtils;
 import io.restassured.response.Response;
 
@@ -13,6 +14,10 @@ public class CommissionService {
     // commission controller constants
     public final String GET_AGENCY_COMMISSION_BY_ID_AND_LOB = "/commissions/carriers/{id}/lob/{lob}";
     public final String GET_PRODUCER_COMMISSION_BY_ID = "/commissions/producers/{id}";
+
+    // default rule controller constants
+    public final String GET_DEFAULT_RULE = "/rules/default";
+    public final String PUT_DEFAULT_RULE = "/rules/default";
 
     private ServiceUtils serviceUtils;
 
@@ -32,6 +37,17 @@ public class CommissionService {
         String hydratedURL = hydrateURL(GET_PRODUCER_COMMISSION_BY_ID, "{id}", id);
         Response response = serviceUtils.sendGetRequest(hydratedURL);
         return response.getBody().jsonPath().getObject("content", ProducerCommissionV1.class);
+    }
+
+    // default rule API calls
+    public DefaultRuleV1 getDefaultRule(){
+        Response response = serviceUtils.sendGetRequest(GET_DEFAULT_RULE);
+        return response.getBody().jsonPath().getObject("content", DefaultRuleV1.class);
+    }
+
+    public DefaultRuleV1 putDefaultRule(DefaultRuleV1 requestBody){
+        Response response = serviceUtils.sendPutRequest(PUT_DEFAULT_RULE, requestBody);
+        return response.getBody().jsonPath().getObject("content", DefaultRuleV1.class);
     }
 
     // helper methods
