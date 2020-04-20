b65fe5b4f28baf4746b1c5b14426b28991bbc6f3
nate <npappenhagen@vertafore.com>
2020-04-20 16:00:06 -0600

    debugging CallAnApi sequence

--- src/main/java/com/vertafore/test/tasks/GetTitanAuthToken.java
+++ src/main/java/com/vertafore/test/tasks/GetTitanAuthToken.java
@@ -2,16 +2,16 @@ package com.vertafore.test.tasks;
 
 import static com.vertafore.test.abilities.Authenticate.passwordForAuthenticatedActor;
 import static com.vertafore.test.abilities.Authenticate.usernameForAuthenticatedActor;
-import static net.serenitybdd.rest.SerenityRest.rest;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 import com.vertafore.test.abilities.Authenticate;
-import com.vertafore.test.abilities.HaveTitanContext;
 import com.vertafore.test.models.auth.IDPUserV1;
 import io.restassured.http.ContentType;
+import java.util.List;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.rest.interactions.Post;
 
 // This class is deliberately built seperate from UseAuthServiceTo because we need this getToken
 // endpoint for set-up and this allows us to build the UseAuthServiceTo
@@ -28,11 +28,9 @@ public class GetTitanAuthToken implements Performable {
     user.setPassword(passwordForAuthenticatedActor(actor));
 
     // send request off
-    rest()
-        .with()
-        .contentType(ContentType.JSON)
-        .body(user)
-        .post(HaveTitanContext.theDomainURIOf(actor) + TOKEN_PATH);
+    Post.to(TOKEN_PATH)
+        .with(List.of(req -> req.body(user), req -> req.contentType(ContentType.JSON)))
+        .performAs(actor);
 
     accessToken = SerenityRest.lastResponse().getBody().jsonPath().getString("content.accessToken");
 
