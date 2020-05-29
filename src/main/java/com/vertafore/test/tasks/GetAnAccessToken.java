0eb51acb020ecfc3b257391e24b975756a9cf475
Angela Duran <aduran@vertafore.com>
2020-05-29 12:05:42 -0600

    Cleaned up some files. Adjusted the api and import statement mustache files to reflect new HaveAToken class. Still working on things.

--- src/main/java/com/vertafore/test/tasks/GetAnAccessToken.java
+++ src/main/java/com/vertafore/test/tasks/GetAnAccessToken.java
@@ -1,31 +1,31 @@
-package com.vertafore.test.tasks;
-
-import static com.vertafore.test.abilities.HaveALoginKey.contextForActor;
-import static net.serenitybdd.screenplay.Tasks.instrumented;
-
-import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.Performable;
-
-public class GetAnAccessToken implements Performable {
-
-  @Override
-  public <T extends Actor> void performAs(T actor) {
-    actor.attemptsTo(GetALoginKey.forActor());
-    String context = contextForActor(actor);
-
-    switch (context) {
-      case "appContext":
-        actor.attemptsTo(GetAnAppToken.forActor());
-        break;
-      case "adminContext":
-        actor.attemptsTo(GetAnAdminToken.forActor());
-        break;
-      default:
-        actor.attemptsTo(GetAUserToken.forActor());
-    }
-  }
-
-  public static GetAnAccessToken forActor() {
-    return instrumented(GetAnAccessToken.class);
-  }
-}
+// package com.vertafore.test.tasks;
+//
+// import static com.vertafore.test.abilities.HaveALoginKey.contextForActor;
+// import static net.serenitybdd.screenplay.Tasks.instrumented;
+//
+// import net.serenitybdd.screenplay.Actor;
+// import net.serenitybdd.screenplay.Performable;
+//
+// public class GetAnAccessToken implements Performable {
+//
+//  @Override
+//  public <T extends Actor> void performAs(T actor) {
+//    actor.attemptsTo(GetALoginKey.forActor());
+//    String context = contextForActor(actor);
+//
+//    switch (context) {
+//      case "appContext":
+//        actor.attemptsTo(GetAnAppToken.forActor());
+//        break;
+//      case "adminContext":
+//        actor.attemptsTo(GetAnAdminToken.forActor());
+//        break;
+//      default:
+//        actor.attemptsTo(GetAUserToken.forActor());
+//    }
+//  }
+//
+//  public static GetAnAccessToken forActor() {
+//    return instrumented(GetAnAccessToken.class);
+//  }
+// }
