1bc44c792d5d855d0e13c0386d1f53749eb34d35
Angela Duran <aduran@vertafore.com>
2020-06-15 14:43:10 -0600

    Fixed my super dumb mistake, thanks for your patience lol

--- src/main/java/com/vertafore/test/util/Env_var.java
+++ src/main/java/com/vertafore/test/util/Env_var.java
@@ -11,16 +11,17 @@ public class Env_var {
   public static final String APP_APP_KEY = getProperty("site_db.appkey_app");
   public static final String ADMIN_APP_KEY = getProperty("site_db.appkey_admin");
   public static final String VERT_APP_KEY = getProperty("site_db.appkey_vert");
-  public static final String APP_ACCESS_TO_AGENCY_KEY = "site_db.app_access_to_agency_key";
-  public static final String LOGIN_USER_PATH = "login_user_path";
-  public static final String LOGIN_APP_PATH = "login_app_path";
-  public static final String LOGIN_ADMIN_PATH = "login_admin_path";
-  public static final String USERNAME = "ams.username";
-  public static final String PASSWORD = "ams.password";
-  public static final String VSSO_USERNAME = "ams.vsso_user";
-  public static final String VSSO_PASSWORD = "ams.vsso_password";
+  public static final String APP_ACCESS_TO_AGENCY_KEY =
+      getProperty("site_db.app_access_to_agency_key");
+  public static final String LOGIN_USER_PATH = getProperty("login_user_path");
+  public static final String LOGIN_APP_PATH = getProperty("login_app_path");
+  public static final String LOGIN_ADMIN_PATH = getProperty("login_admin_path");
+  public static final String USERNAME = getProperty("ams.username");
+  public static final String PASSWORD = getProperty("ams.password");
+  public static final String VSSO_USERNAME = getProperty("ams.vsso_user");
+  public static final String VSSO_PASSWORD = getProperty("ams.vsso_password");
   public static final String AGENCY_NO = getProperty("ams.agency_no");
-  public static final String ADMIN_EMP_CODE = "ams.admin_emp_code";
+  public static final String ADMIN_EMP_CODE = getProperty("ams.admin_emp_code");
 
   private static String getProperty(String property) {
     return EnvironmentSpecificConfiguration.from(
