fdeddbaa390f7dedd95de06d90b500743f6888f9
Anna Kharlamova <akharlamova@vertafore.com>
2020-07-06 21:22:37 +0000

    TC-180

---
+++ src/test/java/LOB/LOB.java
@@ -0,0 +1,19 @@
+package LOB;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LOB {
+  @JsonProperty("description")
+  private String description;
+
+  @JsonProperty("lineofBusinessCode")
+  private String lineofBusinessCode;
+
+  @JsonProperty("sdeCode")
+  private String sdeCode;
+
+  @JsonProperty("sdeDescription")
+  private String sdeDescription;
+
+  public LOB() {}
+}
