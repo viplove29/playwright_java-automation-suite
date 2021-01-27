28cd1eaf442b3d6530235070abf65abf8949e14c
Angela Duran <aduran@vertafore.com>
2021-01-27 11:53:41 -0700

    Maybe screwed up intellij...

--- src/main/java/com/vertafore/test/databases/SiteDB.java
+++ src/main/java/com/vertafore/test/databases/SiteDB.java
@@ -1,10 +1,11 @@
 package com.vertafore.test.databases;
 
-import net.serenitybdd.core.Serenity;
+import static com.vertafore.test.util.EnvVariables.*;
 
 import java.sql.Connection;
-
-import static com.vertafore.test.util.EnvVariables.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import net.serenitybdd.core.Serenity;
 
 public class SiteDB extends SharedDB {
 
@@ -19,10 +20,19 @@ public class SiteDB extends SharedDB {
   public static String getUserAppKey(String context) {
     openDB();
     Connection connection = Serenity.sessionVariableCalled(SITEDB_DATABASE);
-    if (connection == null) { return USER_APP_KEY;
-    } else {
+    if (connection == null) {
       return USER_APP_KEY;
+    } else {
+      String appId = "56DFA3AD-CD0D-4C69-9DBD-7E127B748828";
+      String getRowQuery = "SELECT * FROM [dbo].[EMS_AppKey] WHERE AppId = '" + appId + "'";
+      try {
+        ResultSet resultSet = connection.createStatement().executeQuery(getRowQuery);
+        resultSet.next();
+        return resultSet.getString("AppKey");
+      } catch (SQLException e) {
+        System.out.println(getRowQuery);
+        throw new RuntimeException(e);
+      }
     }
   }
-
 }
