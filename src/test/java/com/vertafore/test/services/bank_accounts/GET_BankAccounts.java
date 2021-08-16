77cd7feb7bb33af8a47f0da0ecf3472437a42fb5
Bailee Mulholland <bmulholland@vertafore.com>
2021-08-16 14:24:42 +0000

    Get customer agencies

--- src/test/java/com/vertafore/test/services/bank_accounts/GET_BankAccounts.java
+++ src/test/java/com/vertafore/test/services/bank_accounts/GET_BankAccounts.java
@@ -1,8 +1,14 @@
 package com.vertafore.test.services.bank_accounts;
 
+import static com.vertafore.test.actor.BuildEMSCast.GetAnAccessToken;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.vertafore.test.models.EMSActor;
 import com.vertafore.test.models.ems.BankAccountResponse;
 import com.vertafore.test.servicewrappers.UseBankAccountsTo;
+import java.util.ArrayList;
+import java.util.List;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
@@ -12,85 +18,74 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import static com.vertafore.test.actor.BuildEMSCast.GetAnAccessToken;
-import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RunWith(SerenityRunner.class)
 public class GET_BankAccounts {
 
-    private List<EMSActor> actors = new ArrayList<>();
+  private List<EMSActor> actors = new ArrayList<>();
 
-    @Before
-    public void getAnAccessToken() {
-        actors.addAll(
-                List.of(
-                        new EMSActor().called("bob").withContext("userContext"),
-                        new EMSActor().called("doug").withContext("appContext"),
-                        new EMSActor().called("adam").withContext("adminContext")));
-        OnStage.setTheStage(GetAnAccessToken(actors));
-    }
+  @Before
+  public void getAnAccessToken() {
+    actors.addAll(
+        List.of(
+            new EMSActor().called("bob").withContext("userContext"),
+            new EMSActor().called("doug").withContext("appContext"),
+            new EMSActor().called("adam").withContext("adminContext")));
+    OnStage.setTheStage(GetAnAccessToken(actors));
+  }
 
-    /* This is a basic smoke test that checks the GET /bank-accounts endpoint against user, app and admin
-    context - as well as ensuring the endpoint is returning bank data with the showHidden flag set to both
-    true as well as false.
-     */
-    @Test
-    public void banksReturnsAllBanks() {
+  /* This is a basic smoke test that checks the GET /bank-accounts endpoint against user, app and admin
+  context - as well as ensuring the endpoint is returning bank data with the showHidden flag set to both
+  true as well as false.
+   */
+  @Test
+  public void banksReturnsAllBanks() {
 
-        Actor bob = theActorCalled("bob");
-        Actor doug = theActorCalled("doug");
-        Actor adam = theActorCalled("adam");
+    Actor bob = theActorCalled("bob");
+    Actor doug = theActorCalled("doug");
+    Actor adam = theActorCalled("adam");
 
-        UseBankAccountsTo bankAccountsAPI = new UseBankAccountsTo();
+    UseBankAccountsTo bankAccountsAPI = new UseBankAccountsTo();
 
-        //Testing with showHidden set to true
+    // Testing with showHidden set to true
 
-        adam.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(true, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
+    adam.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(true, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
 
-        doug.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(true, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+    doug.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(true, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-        bob.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(true, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+    bob.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(true, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
+    List<BankAccountResponse> banksHiddenTrue =
+        LastResponse.received()
+            .answeredBy(bob)
+            .getBody()
+            .jsonPath()
+            .getList("", BankAccountResponse.class);
 
-        List<BankAccountResponse> banksHiddenTrue =
-                LastResponse.received()
-                        .answeredBy(bob)
-                        .getBody()
-                        .jsonPath()
-                        .getList("", BankAccountResponse.class);
+    assertThat(banksHiddenTrue.get(0).getClass().getDeclaredFields().length).isEqualTo(4);
+    assertThat(banksHiddenTrue.size()).isGreaterThan(0);
 
-        assertThat(banksHiddenTrue.get(0).getClass().getDeclaredFields().length).isEqualTo(4);
-        assertThat(banksHiddenTrue.size()).isGreaterThan(0);
+    // Testing with showHidden set to false
 
-        //Testing with showHidden set to false
+    adam.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(false, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
 
-        adam.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(false, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
+    doug.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(false, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-        doug.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(false, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+    bob.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(false, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-        bob.attemptsTo(bankAccountsAPI.GETBankAccountsOnTheBankaccountsController(false, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+    List<BankAccountResponse> banksHiddenFalse =
+        LastResponse.received()
+            .answeredBy(bob)
+            .getBody()
+            .jsonPath()
+            .getList("", BankAccountResponse.class);
 
-
-        List<BankAccountResponse> banksHiddenFalse =
-                LastResponse.received()
-                        .answeredBy(bob)
-                        .getBody()
-                        .jsonPath()
-                        .getList("", BankAccountResponse.class);
-
-        assertThat(banksHiddenFalse.get(0).getClass().getDeclaredFields().length).isEqualTo(4);
-        assertThat(banksHiddenFalse.size()).isGreaterThan(0);
-
-    }
+    assertThat(banksHiddenFalse.get(0).getClass().getDeclaredFields().length).isEqualTo(4);
+    assertThat(banksHiddenFalse.size()).isGreaterThan(0);
+  }
 }
--- src/test/java/com/vertafore/test/services/brokers/GET_Brokers.java
+++ src/test/java/com/vertafore/test/services/brokers/GET_Brokers.java
@@ -1,9 +1,14 @@
 package com.vertafore.test.services.brokers;
 
+import static com.vertafore.test.actor.BuildEMSCast.GetAnAccessToken;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.vertafore.test.models.EMSActor;
 import com.vertafore.test.models.ems.BrokerResponse;
-import com.vertafore.test.models.ems.PagingResponseBrokerResponse;
 import com.vertafore.test.servicewrappers.UseBrokersTo;
+import java.util.ArrayList;
+import java.util.List;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
@@ -13,66 +18,54 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.vertafore.test.actor.BuildEMSCast.GetAnAccessToken;
-import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RunWith(SerenityRunner.class)
 public class GET_Brokers {
 
-    private List<EMSActor> actors = new ArrayList<>();
+  private List<EMSActor> actors = new ArrayList<>();
 
-    @Before
-    public void getAnAccessToken() {
-        actors.addAll(
-                List.of(
-                        new EMSActor().called("bob").withContext("userContext"),
-                        new EMSActor().called("doug").withContext("appContext"),
-                        new EMSActor().called("adam").withContext("adminContext")));
-        OnStage.setTheStage(GetAnAccessToken(actors));
-    }
+  @Before
+  public void getAnAccessToken() {
+    actors.addAll(
+        List.of(
+            new EMSActor().called("bob").withContext("userContext"),
+            new EMSActor().called("doug").withContext("appContext"),
+            new EMSActor().called("adam").withContext("adminContext")));
+    OnStage.setTheStage(GetAnAccessToken(actors));
+  }
 
-    /* This is a simple smoke test that sends a request to get the first 5 brokers and checks that the
-    response body is not empty as well as the correct number of fields being returned.
+  /* This is a simple smoke test that sends a request to get the first 5 brokers and checks that the
+  response body is not empty as well as the correct number of fields being returned.
+   */
+  @Test
+  public void brokersReturnsAllBrokers() {
+
+    Actor bob = theActorCalled("bob");
+    Actor doug = theActorCalled("doug");
+    Actor adam = theActorCalled("adam");
+
+    UseBrokersTo brokersAPI = new UseBrokersTo();
+
+    /*Send requests with the skip value set to 0, and top as well as totalRecords set to 5
+    to return only first 5 results.
      */
-    @Test
-    public void brokersReturnsAllBrokers() {
 
-        Actor bob = theActorCalled("bob");
-        Actor doug = theActorCalled("doug");
-        Actor adam = theActorCalled("adam");
+    adam.attemptsTo(brokersAPI.GETBrokersOnTheBrokersController(0, 5, 5, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
 
-        UseBrokersTo brokersAPI = new UseBrokersTo();
+    doug.attemptsTo(brokersAPI.GETBrokersOnTheBrokersController(0, 5, 5, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
+    bob.attemptsTo(brokersAPI.GETBrokersOnTheBrokersController(0, 5, 5, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-        /*Send requests with the skip value set to 0, and top as well as totalRecords set to 5
-        to return only first 5 results.
-         */
+    List<BrokerResponse> brokerResponse =
+        LastResponse.received()
+            .answeredBy(bob)
+            .getBody()
+            .jsonPath()
+            .getList("response", BrokerResponse.class);
 
-        adam.attemptsTo(brokersAPI.GETBrokersOnTheBrokersController(0, 5, 5, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
-
-        doug.attemptsTo(brokersAPI.GETBrokersOnTheBrokersController(0, 5, 5, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
-
-        bob.attemptsTo(brokersAPI.GETBrokersOnTheBrokersController(0, 5, 5, "string"));
-        assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
-
-
-        List<BrokerResponse> brokerResponse  =
-                LastResponse.received()
-                    .answeredBy(bob)
-                    .getBody()
-                    .jsonPath()
-                    .getList("response", BrokerResponse.class);
-
-       assertThat(brokerResponse.get(0).getClass().getDeclaredFields().length).isEqualTo(8);
-       assertThat(brokerResponse.size()).isGreaterThan(0);
-
-    }
-
-
+    assertThat(brokerResponse.get(0).getClass().getDeclaredFields().length).isEqualTo(8);
+    assertThat(brokerResponse.size()).isGreaterThan(0);
+  }
 }
