9fbe74130e31f899dcc5f0f124c04ea7f31906bc
Bailee Mulholland <bmulholland@vertafore.com>
2021-09-22 19:50:42 +0000

    Update get company addresses

--- src/test/java/com/vertafore/test/services/companies/GET_CompanyAddresses.java
+++ src/test/java/com/vertafore/test/services/companies/GET_CompanyAddresses.java
@@ -9,6 +9,7 @@ import com.vertafore.test.models.ems.CompanyAddressResponse;
 import com.vertafore.test.servicewrappers.UseCompanyAddressesTo;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
@@ -34,6 +35,13 @@ public class GET_CompanyAddresses {
     OnStage.setTheStage(GetAnAccessToken(actors));
   }
 
+  // Helpers
+  public static String randCompanyCode = "";
+  public static String randCompanyAddress = "";
+
+  Random random = new Random();
+  int randomInt = random.nextInt(100);
+
   @Test
   public void companyAddressesReturnsAllCompanyAddresses() {
     Actor bob = theActorCalled("bob");
@@ -42,10 +50,6 @@ public class GET_CompanyAddresses {
 
     UseCompanyAddressesTo companyAddressesAPI = new UseCompanyAddressesTo();
 
-    bob.attemptsTo(
-        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated(null, "string"));
-    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
-
     doug.attemptsTo(
         companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated(null, "string"));
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
@@ -53,29 +57,29 @@ public class GET_CompanyAddresses {
     adam.attemptsTo(
         companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated(null, "string"));
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
-  }
-
-  @Test
-  public void companyAddressesReturnsOneCompanyAddress() {
-    Actor bob = theActorCalled("bob");
-    Actor doug = theActorCalled("doug");
-    Actor adam = theActorCalled("adam");
-
-    UseCompanyAddressesTo companyAddressesAPI = new UseCompanyAddressesTo();
-
-    doug.attemptsTo(
-        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated("!!%", "string"));
-    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
-
-    adam.attemptsTo(
-        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated("!!%", "string"));
-    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
 
     bob.attemptsTo(
-        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated("!!%", "string"));
+        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated(null, "string"));
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-    SerenityRest.lastResponse().prettyPrint();
+    List<CompanyAddressResponse> companyAddressResponse =
+        LastResponse.received()
+            .answeredBy(bob)
+            .getBody()
+            .jsonPath()
+            .getList("", CompanyAddressResponse.class);
+
+    assertThat(companyAddressResponse.get(0).getClass().getDeclaredFields().length).isEqualTo(3);
+    assertThat(companyAddressResponse.size()).isGreaterThan(0);
+
+    randCompanyCode = companyAddressResponse.get(randomInt).getCompanyCode();
+    randCompanyAddress = companyAddressResponse.get(randomInt).getCompanyAddressId();
+
+    /*Test to validate that one Company Address can be found*/
+    bob.attemptsTo(
+        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated(
+            randCompanyCode, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
     CompanyAddressResponse companyAddress =
         LastResponse.received()
@@ -90,23 +94,39 @@ public class GET_CompanyAddresses {
     assertThat(companyAddress.getClass().getDeclaredFields().length).isEqualTo(3);
 
     // Response body field data assertions
-    assertThat(companyAddress.getCompanyAddressId())
-        .isEqualTo("b7278590-8f0e-4406-b79f-fad801e41ccc");
+    assertThat(companyAddress.getCompanyAddressId()).isEqualTo(randCompanyAddress);
     assertThat(companyAddress.getEmail() == null).isTrue();
   }
 
   @Test
   @WithTag("19R2")
-  public void companyAddressesReturnsCompanyAddresses19R2() {
+  public void companyAddressesReturnsAllCompanyAddresses19R2() {
     Actor mary = theActorCalled("mary");
 
     UseCompanyAddressesTo companyAddressesAPI = new UseCompanyAddressesTo();
 
     mary.attemptsTo(
-        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated("!!%", "string"));
+        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated(null, "string"));
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-    SerenityRest.lastResponse().prettyPrint();
+    List<CompanyAddressResponse> companyAddresses =
+        LastResponse.received()
+            .answeredBy(mary)
+            .getBody()
+            .jsonPath()
+            .getList("", CompanyAddressResponse.class);
+
+    assertThat(companyAddresses.get(0).getClass().getDeclaredFields().length).isEqualTo(3);
+    assertThat(companyAddresses.size()).isGreaterThan(0);
+
+    randCompanyCode = companyAddresses.get(randomInt).getCompanyCode();
+    randCompanyAddress = companyAddresses.get(randomInt).getCompanyAddressId();
+
+    /*Test to validate that in 19R2 one Company address can be found*/
+    mary.attemptsTo(
+        companyAddressesAPI.GETCompanyAddressesOnTheCompaniesControllerDeprecated(
+            randCompanyCode, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
     CompanyAddressResponse companyAddress =
         LastResponse.received()
@@ -121,8 +141,7 @@ public class GET_CompanyAddresses {
     assertThat(companyAddress.getClass().getDeclaredFields().length).isEqualTo(3);
 
     // Response body field data assertions
-    assertThat(companyAddress.getCompanyAddressId())
-        .isEqualTo("b7278590-8f0e-4406-b79f-fad801e41ccc");
+    assertThat(companyAddress.getCompanyAddressId()).isEqualTo(randCompanyAddress);
     assertThat(companyAddress.getEmail() == null).isTrue();
   }
 }
