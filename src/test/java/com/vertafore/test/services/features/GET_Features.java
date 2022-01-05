4e951560a8af1c84fd26d656d9646ff4f9c9f46b
Jorit Pfleiderer <jpfleiderer@vertafore.com>
2022-01-05 18:37:44 +0000

    Fix Features TC to not use hardcoded data

--- src/test/java/com/vertafore/test/services/features/GET_Features.java
+++ src/test/java/com/vertafore/test/services/features/GET_Features.java
@@ -5,6 +5,7 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.vertafore.test.models.EMSActor;
+import com.vertafore.test.models.ems.FeatureAvailabilityResponse;
 import com.vertafore.test.servicewrappers.UseFeaturesTo;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.rest.questions.LastResponse;
 import net.thucydides.core.annotations.WithTag;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,46 +23,100 @@ import org.junit.runner.RunWith;
 public class GET_Features {
   private List<EMSActor> actors = new ArrayList<>();
 
-  // The three actors used in this test are examples of optional params that can be used when
-  // building your actors. The first two are specifically for use with "userContext". The first,
-  // "bob", uses the project's default VSSO user. The second, "fred", uses AMS login credentials
-  // that are different from the project's default user credentials. In this case it is not
-  // necessary to indicate whether it is a native or VSSO login, as it doesn't matter in terms of
-  // getting a token. The
-  // third, "mary" does not have a context indicated - in this case the project will automatically
-  // use the deprecated auth endpoints. "mary" also has a version indicated - if this is not
-  // included then by default the
-  // project will use the current working version in QA. This will be especially useful if you need
-  // to test earlier versions of
-  // EMS. Without using a context you can still use the default vsso user, or different credentials.
+  /* The three additional actors used in this test are examples of optional params that can be used when
+  building your actors. The first two are specifically for use with "userContext". The first,
+  "bob", uses the project's default VSSO user. The second, "fred", uses AMS login credentials
+  that are different from the project's default user credentials. In this case it is not
+  necessary to indicate whether it is a native or VSSO login, as it doesn't matter in terms of
+  getting a token. The third, "mary" does not have a context indicated - in this case the project
+  will automatically use the deprecated auth endpoints. "mary" also has a version indicated - if
+  this is not included then by default the project will use the current working version in QA.
+  This will be especially useful if you need to test earlier versions of EMS.
+  Without using a context you can still use the default vsso user, or different credentials.
+   */
 
   @Before
   public void getAnAccessToken() {
     actors.addAll(
         List.of(
+            new EMSActor().called("doug").withContext("appContext"),
+            new EMSActor().called("adam").withContext("adminContext"),
             new EMSActor().called("bob").withContext("userContext").withLoginType("vsso"),
+            new EMSActor().called("mary").withVersion("19R2"),
             new EMSActor()
                 .called("fred")
                 .withContext("userContext")
-                .withUsername("qaown")
-                .withPassword("P@ssw0rd2"),
-            new EMSActor().called("mary").withVersion("19R2")));
+                .withUsername("admin")
+                .withPassword("AMS4all!")));
     OnStage.setTheStage(GetAnAccessToken(actors));
   }
 
+  /* The purpose of this test is to hit the Get /features endpoint using admin, user, and app context -
+   as well as an actor with vsso login and an actor with a custom username + password combination.
+   The test validates proper response codes, and then captures a features object used to query through the
+  same endpoint again. By doing so the data validations are not hardcoded.
+   */
   @Test
   public void featuresReturnsAllFeatures() {
     Actor bob = theActorCalled("bob");
+    Actor doug = theActorCalled("doug");
+    Actor adam = theActorCalled("adam");
     Actor fred = theActorCalled("fred");
 
     UseFeaturesTo featuresApi = new UseFeaturesTo();
 
+    fred.attemptsTo((featuresApi.GETFeaturesOnTheFeaturesController()));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+
+    adam.attemptsTo((featuresApi.GETFeaturesOnTheFeaturesController()));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
+
+    doug.attemptsTo((featuresApi.GETFeaturesOnTheFeaturesController()));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+
     bob.attemptsTo((featuresApi.GETFeaturesOnTheFeaturesController()));
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-    fred.attemptsTo((featuresApi.GETFeaturesOnTheFeaturesController()));
+    FeatureAvailabilityResponse featureResponse =
+        LastResponse.received()
+            .answeredBy(bob)
+            .getBody()
+            .jsonPath()
+            .getList("", FeatureAvailabilityResponse.class)
+            .get(0);
+
+    assertThat(featureResponse.getClass().getDeclaredFields().length).isEqualTo(3);
+
+    String name = featureResponse.getName();
+    Boolean agencyEnabled = featureResponse.getAgencyEnabled();
+    Boolean globalEnabled = featureResponse.getGlobalEnabled();
+
+    adam.attemptsTo(featuresApi.GETFeaturesFeatureNameOnTheFeaturesController(name, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
+
+    doug.attemptsTo(featuresApi.GETFeaturesFeatureNameOnTheFeaturesController(name, "string"));
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+
+    bob.attemptsTo(featuresApi.GETFeaturesFeatureNameOnTheFeaturesController(name, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+
+    FeatureAvailabilityResponse featureCheck =
+        LastResponse.received()
+            .answeredBy(bob)
+            .getBody()
+            .jsonPath()
+            .getObject("", FeatureAvailabilityResponse.class);
+
+    assertThat(featureResponse.getClass().getDeclaredFields().length).isEqualTo(3);
+
+    assertThat(featureCheck.getName()).isEqualTo(name);
+    assertThat(featureCheck.getAgencyEnabled()).isEqualTo(agencyEnabled);
+    assertThat(featureCheck.getGlobalEnabled()).isEqualTo(globalEnabled);
   }
+
+  /* The purpose of this test is the same as above, but only uses the user context and is ran against
+  a 19R2 agency.
+   */
 
   @Test
   @WithTag("19R2")
@@ -71,5 +127,35 @@ public class GET_Features {
 
     mary.attemptsTo((featuresApi.GETFeaturesOnTheFeaturesController()));
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+
+    FeatureAvailabilityResponse featureResponse =
+        LastResponse.received()
+            .answeredBy(mary)
+            .getBody()
+            .jsonPath()
+            .getList("", FeatureAvailabilityResponse.class)
+            .get(0);
+
+    assertThat(featureResponse.getClass().getDeclaredFields().length).isEqualTo(3);
+
+    String name = featureResponse.getName();
+    Boolean agencyEnabled = featureResponse.getAgencyEnabled();
+    Boolean globalEnabled = featureResponse.getGlobalEnabled();
+
+    mary.attemptsTo(featuresApi.GETFeaturesFeatureNameOnTheFeaturesController(name, "string"));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+
+    FeatureAvailabilityResponse featureCheck =
+        LastResponse.received()
+            .answeredBy(mary)
+            .getBody()
+            .jsonPath()
+            .getObject("", FeatureAvailabilityResponse.class);
+
+    assertThat(featureResponse.getClass().getDeclaredFields().length).isEqualTo(3);
+
+    assertThat(featureCheck.getName()).isEqualTo(name);
+    assertThat(featureCheck.getAgencyEnabled()).isEqualTo(agencyEnabled);
+    assertThat(featureCheck.getGlobalEnabled()).isEqualTo(globalEnabled);
   }
 }
