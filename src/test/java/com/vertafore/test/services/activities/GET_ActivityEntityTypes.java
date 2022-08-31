e1413bea7c0569f7fe8931c078be53eaf44a4ec7
Allen Roisen-Reinoso <aroisen-reinoso@vertafore.com>
2022-08-31 14:42:37 +0000

    GET Activity/Entity-Types Baseline Test and deprecated endpoint refactor

--- src/test/java/com/vertafore/test/services/activities/GET_ActivityEntityTypes.java
+++ src/test/java/com/vertafore/test/services/activities/GET_ActivityEntityTypes.java
@@ -4,10 +4,9 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.vertafore.test.actor.TokenSuperClass;
-import com.vertafore.test.models.ems.ActivityIdResponse;
-import com.vertafore.test.models.ems.SpecificActivityResponse;
+import com.vertafore.test.models.ems.EntityTypeResponse;
 import com.vertafore.test.servicewrappers.UseActivityTo;
-import com.vertafore.test.util.ActivityUtil;
+import java.util.List;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
@@ -16,38 +15,34 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(SerenityRunner.class)
-public class GET_Activity extends TokenSuperClass {
+public class GET_ActivityEntityTypes extends TokenSuperClass {
 
   @Test
-  public void getActivityGetsActivity() {
+  public void getActivityEntityTypesReturnsActivityEntityTypes() {
     Actor AADM_User = theActorCalled("AADM_User");
     Actor ORAN_App = theActorCalled("ORAN_App");
     Actor VADM_Admin = theActorCalled("VADM_Admin");
 
-    // stage data
-    ActivityIdResponse activityPosted = ActivityUtil.postRandomActivity(AADM_User);
-    String activityId = activityPosted.getActivityId();
-
     UseActivityTo activityApi = new UseActivityTo();
 
-    ORAN_App.attemptsTo(activityApi.GETActivityOnTheActivitiesController(activityId, ""));
+    ORAN_App.attemptsTo(activityApi.GETActivityEntityTypesOnTheActivitiesController());
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-    VADM_Admin.attemptsTo(activityApi.GETActivityOnTheActivitiesController(activityId, ""));
+    VADM_Admin.attemptsTo(activityApi.GETActivityEntityTypesOnTheActivitiesController());
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
 
-    AADM_User.attemptsTo(activityApi.GETActivityOnTheActivitiesController(activityId, ""));
+    AADM_User.attemptsTo(activityApi.GETActivityEntityTypesOnTheActivitiesController());
     assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
 
-    SpecificActivityResponse activityResponse =
+    List<EntityTypeResponse> entityTypeResponse =
         LastResponse.received()
             .answeredBy(AADM_User)
             .getBody()
             .jsonPath()
-            .getObject("", SpecificActivityResponse.class);
+            .getList("", EntityTypeResponse.class);
 
-    assertThat(activityResponse).isNotNull();
-    assertThat(activityResponse.getClass().getDeclaredFields().length).isEqualTo(7);
-    assertThat(activityResponse.getActivityId()).isEqualTo(activityId);
+    assertThat(entityTypeResponse).isNotNull();
+    assertThat(entityTypeResponse.size()).isGreaterThan(0);
+    assertThat(entityTypeResponse.get(0).getEntityType()).isNotNull();
   }
 }
