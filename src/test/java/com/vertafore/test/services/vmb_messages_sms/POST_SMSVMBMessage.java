9f8d963e0eedb2da7ca8605a4e42378ee1b2bc53
Sruthi Komirishetty <skomirishetty@vertafore.com>
2023-09-07 22:55:59 +0000

    POST_SMSVMBMessage POST_SMSVMBMessageUpdate

---
+++ src/test/java/com/vertafore/test/services/vmb_messages_sms/POST_SMSVMBMessage.java
@@ -0,0 +1,61 @@
+package com.vertafore.test.services.vmb_messages_sms;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.vertafore.test.actor.TokenSuperClass;
+import com.vertafore.test.models.ems.*;
+import com.vertafore.test.servicewrappers.UseSmsTo;
+import java.util.ArrayList;
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.rest.questions.LastResponse;
+import org.junit.Test;
+
+public class POST_SMSVMBMessage extends TokenSuperClass {
+
+  @Test
+  public void postSMSVMBMessageBaselineTest() {
+
+    Actor AADM_User = theActorCalled("AADM_User");
+    Actor ORAN_App = theActorCalled("ORAN_App");
+    Actor VADM_Admin = theActorCalled("VADM_Admin");
+
+    UseSmsTo smsAPI = new UseSmsTo();
+
+    String randomVertaforeNo = "14255550043";
+    String randomContactNo = "18591111111";
+
+    IncomingVmbMessagePostRequest incomingVmbMessagePostRequest =
+        new IncomingVmbMessagePostRequest();
+
+    incomingVmbMessagePostRequest.setContactNo(randomContactNo);
+    incomingVmbMessagePostRequest.setVertaforeNo(randomVertaforeNo);
+    incomingVmbMessagePostRequest.setStatus("Delivered");
+    incomingVmbMessagePostRequest.setBody("Test Body");
+    incomingVmbMessagePostRequest.setAttachments(new ArrayList<>());
+
+    VADM_Admin.attemptsTo(
+        (smsAPI.POSTSmsVmbMessageOnTheVmbmessagessmsController(incomingVmbMessagePostRequest, "")));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(403);
+
+    ORAN_App.attemptsTo(
+        (smsAPI.POSTSmsVmbMessageOnTheVmbmessagessmsController(incomingVmbMessagePostRequest, "")));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+
+    AADM_User.attemptsTo(
+        (smsAPI.POSTSmsVmbMessageOnTheVmbmessagessmsController(incomingVmbMessagePostRequest, "")));
+    assertThat(SerenityRest.lastResponse().getStatusCode()).isEqualTo(200);
+
+    IncomingVmbMessageResponse vmbMessage =
+        LastResponse.received()
+            .answeredBy(AADM_User)
+            .getBody()
+            .jsonPath()
+            .getObject("", IncomingVmbMessageResponse.class);
+
+    assertThat(vmbMessage).isNotNull();
+    assertThat(vmbMessage.getVertaforeNo()).isEqualTo(randomVertaforeNo);
+    assertThat(vmbMessage.getContactNo()).isEqualTo(randomContactNo);
+  }
+}
